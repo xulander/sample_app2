0c7b64a41e6ded7a5519cbf10d5f94b0d17d6e03
Elvis Xu <git>
2013-12-20 11:24:15 -0800

    First commit. Hassled for hours to fix that damn pg gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAppRevisit::Application.config.secret_token = 'f41eedc8fca2068f97a880a6748fe52f94ef6104185cce51c1bcaf3e606b8917b0fba889ef0377a5072b2f5b88e1cf3f38764cc484101c3cd0d787a7a9a4308d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SampleAppRevisit::Application.config.session_store :cookie_store, key: '_sample_app_revisit_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SampleAppRevisit::Application.config.session_store :active_record_store
