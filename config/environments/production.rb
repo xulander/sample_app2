614c00d9eb6fc910f44fae1fc3f428696a2fb7d2
Elvis Xu <git>
2014-01-11 05:37:58 -0800

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,8 @@ SampleAppRevisit::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  config.force_ssl = true
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
