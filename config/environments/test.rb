73996295a9c6b1c5468479ecbce12a03bea592c9
Elvis Xu <git>
2014-01-11 05:37:11 -0800

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,11 @@ SampleAppRevisit::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+
+  # Speed up tests by lowering bCrypt cost function for this test env
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleAppRevisit::Application.routes.draw do
-  get "users/new"
+
+  resources :users 
 
   root to: 'static_pages#home'
 
